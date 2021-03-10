b2fbf4d29197707bfdbb80930e067284b9e76586
neha <neha.sharawat01@gmail.cim>
2021-03-11 02:37:32 +1300

    first commit

---
+++ src/nz/ac/wgtn/ict/myassignment/MyMathAssignment.java
@@ -0,0 +1,205 @@
+package nz.ac.wgtn.ict.myassignment;
+
+import java.util.InputMismatchException;
+
+class MyMathAssignment {
+
+	/**
+	 * This function will add two given numbers and will return the adition
+	 * 
+	 * @param x : The first operand
+	 * @param y : The second operand
+	 * @return res : the adition of two operands
+	 */
+	public double add(double x, double y) {
+		double res = x + y;
+		double finalRes = Math.round(res * 100000) / 100000.0;
+		return finalRes;
+
+	}
+
+	/**
+	 * This function will subtract two given numbers and will return the subtraction
+	 * 
+	 * @param x : the first operand
+	 * @param y : the second operand
+	 * @return res : the subtraction of two operands
+	 */
+	public double sub(double x, double y) {
+		double res = x - y;
+		double finalRes = Math.round(res * 100000) / 100000.0;
+		return finalRes;
+
+	}
+
+	/**
+	 * This function will multiply two given numbers and will return the
+	 * multiplication
+	 * 
+	 * @param x : the first operand
+	 * @param y : the second operand
+	 * @return res : the multiplication of two operands
+	 */
+	public double mul(double x, double y) {
+
+		double res = x * y;
+		double finalRes = Math.round(res * 100000) / 100000.0;
+		return finalRes;
+	}
+
+	/**
+	 * This function will divide two given numbers and will return the division
+	 * 
+	 * @param x : the first operand
+	 * @param y : the second operand
+	 * @return res : the division of two operands
+	 */
+
+	public double div(double x, double y) {
+		if(y == 0) {
+			throw new IllegalArgumentException("Can't divide by zero");
+		}
+			double res = x / y;
+			double finalRes = Math.round(res * 100000) / 100000.0;
+			return finalRes;
+	}
+	
+	
+	/**
+	 * This function will return the product of two vectors
+	 * 
+	 * @param a : the first operand
+	 * @param b : the second operand
+	 * @return res : the dot product of two operands
+	 */
+	public double[] vectorProduct(double[] a, double[] b) throws Exception {
+		if (a.length != b.length) {
+			throw new IllegalArgumentException("The dimensions have to be equal!");
+		}
+		double[] res = new double[a.length];
+		for (int i = 0; i < a.length; i++) {
+			 res[i] += Math.round(( a[i] * b[i]) * 100000) / 100000.0;
+		}
+		return res;
+	}
+
+	
+	/**
+	 * This function will return the product of a matrix and vector
+	 * 
+	 * @param rows   : for the rows of the matrix
+	 * @param column : for the columns of the matrix
+	 * @return result the products of matrix and vector
+	 */
+
+	public double[] MatrixVectorProduct(double[][] matrix, double[] vector) {
+	
+			int rows = matrix.length;  //rows length,
+			int columns = matrix[0].length;  //column length,
+
+			 if (vector.length != columns) throw new RuntimeException("Illegal matrix dimensions.");
+			 
+			double[] result = new double[rows];
+
+			for (int i = 0; i < rows; i++) { // for every index value in the rows of the matrix
+				double temp = 0;
+				for (int j = 0; j < columns; j++) { // get each index value in each column and...
+					 result[i] += Math.round((matrix[i][j] * vector[j]) * 100000) / 100000.0; // Multiply by the vector, which only has 1 column
+					
+				}
+			}
+			return result ;
+
+	}
+	
+	
+	/**
+	 * This function will return the product of two matrix
+	 * 
+	 * @param m1 : Multiplicand
+	 * @param m2 : Multiplier
+	 * @return mResult : the result products of matrix and vector
+	 */
+	public double[][] multiplyTwoMatrix(double[][] m1, double[][] m2) throws Exception {
+		int m1CLength = m1[0].length; // m1 columns length
+		int m2RLength = m2.length; // m2 rows length
+
+		if (m1CLength != m2RLength) {
+			throw new InputMismatchException("Incompatible matrices. cols of first matrix must equal rows of second matrix");
+
+		} else {
+			int RRLength = m1.length; // m1 result rows length
+
+			int RCLength = m2[0].length; // m2 result columns length
+
+			double[][] mResult = new double[RRLength][RCLength];
+
+			for (int i = 0; i < RRLength; i++) { // rows from m1
+				for (int j = 0; j < RCLength; j++) { // columns from m2
+					for (int k = 0; k < m1CLength; k++) { // columns from m1
+
+						mResult[i][j] += Math.round((m1[i][k] * m2[k][j]) * 100000) / 100000.0;
+					}
+				}
+
+			}
+			return mResult;
+		}
+	}
+
+	/**
+	 * This function will return the transpose of a matrix
+	 * 
+	 * @param m  : for rows length
+	 * @param m2 : Multiplier
+	 * @return mResult : the result products of matrix and vector
+	 */
+
+	public double[][] transposeMatrix(double[][] matrix) {
+		int n = matrix[0].length;   //column length
+		int m = matrix.length;   //rows length
+
+		double[][] transposedMatrix = new double[n][m];
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				transposedMatrix[i][j] = matrix[j][i];
+			}
+		}
+
+		return transposedMatrix;
+	}
+
+	
+	/**
+	 * Checks whether given matrix parameter is an identity matrix. returns boolean
+	 * true or false.
+	 */
+	
+	public boolean IdentityMatrixSourceData(double[][] matrix) {
+		 boolean flag = true ;
+		int n = matrix[0].length;   //n = column length
+		int m = matrix.length;   //m = rows length
+
+		if (m != n) {
+			throw new InputMismatchException("Matrix should be a square matrix");
+
+		} else {   
+			for(int i = 0; i < m; i++){    
+                for(int j = 0; j < n; j++){    
+                  if(i == j && matrix[i][j] != 1){    
+                      flag = false;    
+                      break;    
+                  }    
+                  if(i != j && matrix[i][j] != 0){    
+                      flag = false;    
+                      break;    
+                  }    
+                }    
+            }  
+			return true;
+	}
+	}
+	
+
+}
---
+++ src/nz/ac/wgtn/ict/myassignment/MyMathAssignmentTest.java
@@ -0,0 +1,177 @@
+package nz.ac.wgtn.ict.myassignment;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class MyMathAssignmentTest {
+
+	MyMathAssignment ma = new MyMathAssignment();
+
+	@Test
+	public static Collection addTest1() {
+		return Arrays.asList(new Object[][] { { 1, 1, 2 }, { 3.14, -1.72, 1.42 }, { -7, -1, -8 }});
+	}
+
+	@ParameterizedTest(name = "{index}: add({0}, {1})= {2}")
+	@DisplayName("Addition method with collection")
+	@MethodSource("addTest1")
+	public void addTest(double v1, double v2, double exp) {
+		assertEquals(exp, ma.add(v1, v2), "This method is not working");
+	}
+
+	@Test
+	public static Collection subTest1() {
+		return Arrays.asList(new Object[][] { { -2, -1, -1 }, { 3.14, 1.72, 1.42 }, { 7, 1, 6 } });
+	}
+
+	@ParameterizedTest(name = "{index}: sub({0}, {1})= {2}")
+	@MethodSource("subTest1")
+	public void subTest(double v1, double v2, double exp) {
+		assertEquals(exp, ma.sub(v1, v2), "This method is not working");
+	}
+
+	@Test
+	public static Collection mulTest1() {
+		return Arrays.asList(new Object[][] { { 6, 2, 12 }, { -3, 0, 0 }, { -11.2, 9, -100.8 }});
+	}
+
+	@ParameterizedTest(name = "{index}: mul({0}, {1})= {2}")
+	@MethodSource("mulTest1")
+	public void mulTest(double v1, double v2, double exp) {
+		assertEquals(exp, ma.mul(v1, v2), "This method is not working");
+	}
+
+	@Test
+	public static Collection divTest1() {
+		return Arrays.asList(new Object[][] { { 6, 2, 3 }, { 12, 6, 2 }, { 3.14, -1.72, -1.82558 }});
+	}
+
+	@ParameterizedTest(name = "{index}: div({0}, {1})= {2}")
+	@MethodSource("divTest1")
+	public void divTest(double v1, double v2, double exp) {
+		assertEquals(exp, ma.div(v1, v2), "This method is not working");
+	}
+
+	//-----------test for "divided by 0"-------------
+
+	static Stream<Arguments>  divTestDATA2() {
+		return Stream.of(
+				Arguments.of(6,0));
+	}
+	@ParameterizedTest
+	@MethodSource("divTestDATA2")
+	public void divTest2(double v1, double v2) {
+		assertThrows(IllegalArgumentException.class,() ->ma.div(v1, v2)); 
+	}
+	
+	
+	
+	//-------------------------- Vector product----------------
+	
+	static Stream<Arguments> vectorTestData() {
+			return Stream.of(
+			Arguments.of(new double[] {1,2,3}, new double[] {4, 5, 6}, new double[] {4, 10, 18}),
+			Arguments.of(new double[] {-4,-6,-8}, new double[] {-7, 5, 0}, new double[] {28, -30, 0}),
+			Arguments.of(new double[] {3.7, 1.1, -4.3}, new double[] {8.1, 5.2, 7.6}, new double[] {29.97, 5.72, -32.68}));
+		}
+
+	@ParameterizedTest(name = "{index}: vector({0}, {1} )= {2}")
+	@MethodSource("vectorTestData")
+
+	public void vectorTest1(double[] v1, double[] v2, double[] exp) throws Exception {
+		assertArrayEquals(exp, ma.vectorProduct(v1, v2), "This method is not working");
+	}
+
+	
+	
+	//-----------------------------------Matrix and vector Product----------------------
+	
+	static Stream<Arguments> matrixVectorProductTestData() {
+		return Stream.of(Arguments.of(new double[][] {{2,2,4},{0}}, new double[] {2,0,3}, new double[] {16, 3}),
+				Arguments.of(new double[][] {{3, 4, 2},{1, 3, 1}}, new double[] {4, 7, 1}, new double[] {42,26}), 
+				Arguments.of(new double[][] {{0, 4,-2},{-4, -3, 1}}, new double[] {2, -1, 0}, new double[] {-4, -5}));
+			
+	}
+
+	@ParameterizedTest
+	@MethodSource("matrixVectorProductTestData")
+	public void MatrixVectorTest1(double[][] v1, double[] v2, double[] exp) {
+	
+				assertArrayEquals(exp, ma.MatrixVectorProduct(v1,v2),
+						"This method is not working");
+			
+	}
+
+	//------------------------------------------Matrix and Matrix product----------------------
+	
+	static Stream<Arguments> multiplyTwoMatrixTestData() {
+		return Stream.of(Arguments.of(new double[][] {{1,2,3},{4,5,6}}, new double[][] {{7,8},{9,10}, {11,12}}, new double[][] {{58, 64}, {139, 154}}),
+				Arguments.of(new double[][] {{3.3, 4.3},{-1.1, 1.7}}, new double[][] {{4, 7},{2.2, -1.9}}, new double[][]{ {22.66, 14.93}, {7.5, -5.65}})); 
+	
+	}
+
+	@ParameterizedTest
+	@MethodSource("multiplyTwoMatrixTestData")
+	public void MultiplyTwoMatrixTest1(double[][] v1, double[][] v2, double[][] exp) {
+	
+				
+					try {
+						assertArrayEquals(exp, ma.multiplyTwoMatrix(v1,v2),
+								"This method is not working");
+					} catch (Exception e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				
+	}
+	
+	
+	//------------------------------------------Transpose Matrix-----------------
+	
+	static Stream<Arguments> transposeMatrixTestData() {
+		return Stream.of(Arguments.of(new double[][] {{6, 4, 24},{1, -9, 8}}, new double[][] {{6, 1}, {4, -9}, {24, 8}}));		
+	}
+
+	@ParameterizedTest
+	@MethodSource("transposeMatrixTestData")
+	public void TransposeMatricTest(double[][] v1, double[][] exp) {
+	
+				try {
+					assertArrayEquals(exp, ma.transposeMatrix(v1),
+							"This method is not working");
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}	
+	}
+
+	
+	//-----------------------------------------------Identity Matrix------------------------
+	
+	static Stream<Arguments> IndentityMatrixTestData() {
+		return Stream.of(Arguments.of(new double[][] {{1,0,0},{0,1,0}, {0,0,1}},true));		
+	}
+
+	@ParameterizedTest
+	@MethodSource("IndentityMatrixTestData")
+	public void IndentityMatrixTest(double[][] v1, boolean exp) {
+	
+				try {
+					assertEquals(exp, ma.IdentityMatrixSourceData(v1));
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}	
+	}
+	
+}
